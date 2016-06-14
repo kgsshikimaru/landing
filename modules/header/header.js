4f10c0b416678dd09e5d5bd7bb841cff62e7d794
will-just-42 <WillJust42@gmail.com>
2016-06-14 16:43:19 +0300

    reconstruct header menu - without bootstrap

--- modules/header/header.js
+++ modules/header/header.js
@@ -1,17 +1,28 @@
-;
-'use strict';
+;'use strict';
 
+/*===================================
+=            mobile activate menu            =
+===================================*/
+
+$('.menu__container__activate-menu').on('click', function() {
+    $('.menu__container__navigation').slideToggle('normal');
+});
+
+/*=====  End of mobile activate menu  ======*/
 
 /*=================================================================
-=            fix bootstrap collapsed on resized window            =
+=      fix collapsed menu after <767px => open menu => close close
+=>     resize to large window
 =================================================================*/
 
 $(window).on('resize', function() {
-    if (window.innerWidth > 767) $('#navbar').collapse('show');
-    if (window.innerWidth <= 767) $('#navbar').collapse('hide');
+    if (window.innerWidth > 767) $('.menu__container__navigation').show();
+    if (window.innerWidth <= 767) $('.menu__container__navigation').hide();
 });
 
-/*=====  End of fix bootstrap collapsed on resized window  ======*/
+/*=====  fix collapsed menu after <767px => open menu => close close
+=>     resize to large window
+                                                            ======*/
 
 /*=========================================================================
 =            add class to highlight active and remove siblings            =
