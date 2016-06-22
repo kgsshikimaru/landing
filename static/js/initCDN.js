e2b58efb2469727a7a77a3240e2a8c152b7ae09e
will-just-42 <WillJust42@gmail.com>
2016-06-22 15:17:03 +0300

    speed 99/100

--- static/js/initCDN.js
+++ static/js/initCDN.js
@@ -1,3 +1,4 @@
+import {jsCssLoader} from 'static/js/jsCssLoader';
 
 import {tabsInit} from 'static/js/libraries/tabsInit';
 
@@ -14,108 +15,41 @@ import svg4everybody from 'static/js/separate-js/svg4everybody.min';
 
 
 
-function getXHR() {
-    var xmlHttp;
-    try {
-        xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-        try {
-            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
-        } catch (E) {
-            xmlHttp = false;
-        }
-    }
-
-    if (!xmlHttp && typeof XMLHttpRequest != 'undefined') {
-        xmlHttp = new XMLHttpRequest();
-    }
-
-    return xmlHttp;
-}
-
-function process(url, callback) {  // адрес - куда стучимся
-    var xmlHttp = getXHR();
-
-
-    xmlHttp.open('GET', url, true);
-    xmlHttp.onreadystatechange = function() {
-        if(xmlHttp.readyState == 4) {
-            if( !(xmlHttp.status === 200 || xmlHttp.status === 304) ) {
-                callback()
-            }
-        }
-    };
-    xmlHttp.send(null);
-}
-
-
-
-
-
-function loadCss(...file){
-    for (let i = 0; i < file.length; i++) {
-        let link = document.createElement("link");
-        link.setAttribute("rel", "stylesheet");
-        link.setAttribute("type", "text/css");
-        link.setAttribute("href", file[i]);
-        document.getElementsByTagName("body")[0].appendChild(link)
-    }
-}
-
-function loadJs(...scripts) {
-    return new Promise( (resolve, reject) => {
-        function addScript(src) {
-            let script = document.createElement('script');
-            script.async = true;
-            script.src = src;
-            document.getElementsByTagName('body')[0].appendChild(script);
-
-
-            process(src, () => console.log('callback'));
-
-            return script;
-        }
-        let counter = 0;
-
-        function onload(i) {
-            counter++;
-            if (counter == scripts.length) resolve();
-        }
-
-        for (let i = 0; i < scripts.length; i++) {
-            let script = addScript(scripts[i]);
-
-            script.onload = function() {
-                onload(i);
-            }
-        }
-    })
-}
-
-
-
-
-loadCss(
-    '/static/css/main.min.css',
-    '//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css',
-    '//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css',
-    '//fonts.googleapis.com/css?family=Roboto:400,500,700',
-    '//fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister'
-);
-loadJs("//code.jquery.com/jquery-3.0.0.min.js",
-        "//cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.2/picturefill.min.js")
+jsCssLoader(
+    ['css', ['/static/css/main.min.css', '/static/css/main.css']],
+    ['css', ['//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css',
+             '/static/reservation/slick.min.css']],
+    ['css', ['//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css',
+             '/static/reservation/slick-theme.min.css']],
+    ['css', ['//fonts.googleapis.com/css?family=Roboto:400,500,700',
+             '/static/reservation/roboto.css']],
+    ['css', ['//fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister',
+             '/static/reservation/love.css']],
+    ['js', ['//code.jquery.com/jquery-3.0.0.min.js',
+            '/static/reservation/jquery-3.0.0.min.js']],
+    ['js', ['//cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.2/picturefill.min.js',
+            '/static/reservation/picturefill.min.js']]
+    )
     .then( () => {
         tabsInit();
         header();
         section1();
-        loadJs("//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js")
+        jsCssLoader(
+            ['js', ['//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js',
+                '/static/reservation/slick.min.js']]
+        )
+        .then(() => {
+            section3();
+            section5();
+            svg4everybody();
+        });
     })
-    .then( () => {
+    .catch( (data) => {
+        console.log(data);
         section3();
         section5();
-        svg4everybody();
-        console.log('finish');
-    })
-    .catch( reason =>  {
-        console.error('error' + reason)
     });
+
+
+
+
---
+++ static/js/jsCssLoader.js
@@ -0,0 +1,68 @@
+// jsCssLoader([[type,url,alternativeUrl...],[type,url,alternativeUrl...]...]).then()
+export function jsCssLoader(...arr)  {
+     return new Promise( (resolve, reject) => {
+         let counterOnLoad = 0;
+
+         function ifNeedCallback() {
+             if (counterOnLoad === arr.length) {
+                 resolve()
+             }
+         }
+
+        function addScript(url) {
+            let i = 0;
+                function connectUrl(url) {
+                    let script = document.createElement('script');
+                    script.async = true;
+                    script.src = url[i];
+                    script.onerror = function() {
+                        console.warn('CSS - error on download	' + url[i]);
+                        i++;
+                        if (i < url.length) {
+                            connectUrl(url);
+                        }
+                        if (i === url.length) {
+                            console.error (url[--i] + '     |||--> error on download & not found alternative file for download');
+                        }
+                    };
+                    script.onload = function() {
+                        counterOnLoad++;
+                        ifNeedCallback()
+                    };
+                    document.getElementsByTagName('body')[0].appendChild(script);
+                }
+            connectUrl (url)
+        }
+
+        function addStyle(url) {
+            let i = 0;
+            function connectUrl(url) {
+                let link = document.createElement('link');
+                link.setAttribute('rel', 'stylesheet');
+                link.setAttribute('type', 'text/css');
+                link.setAttribute('href', url[i]);
+                link.onerror = function() {
+                    console.warn('CSS - error on download	' + url[i]);
+                    i++;
+                    if (i < url.length) {
+                        connectUrl(url);
+                    }
+                    if (i === url.length) {
+                        console.error (url[--i] + '     |||--> error on download & not found alternative file for download');
+                    }
+                };
+                link.onload = function() {
+                    counterOnLoad++;
+                    ifNeedCallback()
+                };
+                document.getElementsByTagName('body')[0].appendChild(link);
+            }
+            connectUrl (url)
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            arr[i][0] === 'js' ? addScript(arr[i][1]) : arr[i][0] === 'css' ? addStyle(arr[i][1])
+                : console.log('unknown parameter is : ' + arr[i][0] + '. Use only js/css');
+        }
+    })
+}
